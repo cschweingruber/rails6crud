a8a3767379063d55a559b5854e04be39ec67dcdb
cschweingruber <c.schweingruber@catatec.ch>
2020-10-18 20:43:53 +0200

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -3,5 +3,5 @@ if [ ! -d "/root/.ssh/" ]; then
     mkdir /root/.ssh
     ln -s /var/lib/secrets/is_rsa /root/.ssh/id_rsa
 fi
-rm ${PWD}/tmp/pids/server.pid
+rm /railsapp/tmp/pids/server.pid
 LANG=DE rails s -b 0.0.0.0 >>${PWD}/log/rails_out 2>> ${PWD}/log/rails_err
